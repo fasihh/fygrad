e61e31fec907d37783c2c776af65e42b881156cb
fasihh <fhk.tcs@gmail.com>
2026-03-30 15:32:02 +0500

    Refactor DataLoader to support device management

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import numpy as np
+from node import Device, xp
 
 
 class DataLoader:
@@ -7,17 +7,33 @@ class DataLoader:
         X, y,
         batch_size=32,
         shuffle=True,
+        device: Device = "cpu"
     ):
         self.X = X
         self.y = y
         self.batch_size = batch_size
         self.shuffle = shuffle
+        self.device = device
+
+    def __convert_to_device(self, device: Device):
+        self.X = xp(device).asarray(self.X)
+        self.y = xp(device).asarray(self.y)
+
+    def to_gpu(self):
+        self.__convert_to_device("gpu")
+        self.device = "gpu"
+        return self
+    
+    def to_cpu(self):
+        self.__convert_to_device("cpu")
+        self.device = "cpu"
+        return self
 
     def __iter__(self):
-        idx = np.arange(len(self.X))
+        idx = xp(self.device).arange(len(self.X))
 
         if self.shuffle:
-            np.random.shuffle(idx)
+            xp(self.device).random.shuffle(idx)
 
         for i in range(0, len(idx), self.batch_size):
             batch = idx[i : i + self.batch_size]
