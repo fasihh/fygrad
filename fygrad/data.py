fe8989b9655a163a961762393eb5b04a2659063b
fasihh <fhk.tcs@gmail.com>
2026-05-03 00:08:03 +0500

    fix(data): update string representation to check for last dimension size

--- fygrad/data.py
+++ fygrad/data.py
@@ -95,7 +95,7 @@ class Data:
         return Data(self.xp.matmul(other.data, self.data), device=self.device)
 
     def __str__(self):
-        if len(self.data) == 1:
+        if self.data.shape[-1] == 1:
             return str(self.xp.round(self.data.flatten()[0], 6))
         return str(self.data)
 
